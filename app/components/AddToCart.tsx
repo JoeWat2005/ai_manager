626576ce37cb83e6b0905ee4c5215b7ddeb64cba
Joseph Watmough <watmoughjoe2005@gmail.com>
2026-04-14 19:29:46 +0100

    server side fetching

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react'
+
+export const AddToCart = () => {
+  return (
+    <div>
+        <button onClick={() => console.log('Click')}>Add to Cart</button>
+    </div>
+  )
+}
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { AddToCart } from './AddToCart'
+export const ProductCard = () => {
+  return (
+    <div>
+        <AddToCart />
+    </div>
+  )
+}
